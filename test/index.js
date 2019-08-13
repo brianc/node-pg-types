10f2725d6133cae1a09a52e1a09305747e66fb16
Sehrope Sarkuni <sehrope@jackdb.com>
2019-08-13 09:59:33 -0400

    test: Convert binary test vectors via Buffer.from(...) (#89)
    Convert non-Buffer binary test vectors via Buffer.from(...) to ensure that
    all binary tests are executed on a Buffer object. Previously some tests
    would pass objects that could be accessed as array like objects such as
    strings or arrays.

--- test/index.js
+++ test/index.js
@@ -10,6 +10,13 @@ test('types', function (t) {
       var parser = getTypeParser(type.id, type.format)
       type.tests.forEach(function (tests) {
         var input = tests[0]
+        if (type.format === 'binary' && input !== null && !Buffer.isBuffer(input)) {
+          if (Array.isArray(input) || typeof(input) === 'string') {
+            input = Buffer.from(input)
+          } else {
+            throw new Error('Binary test inputs must be null, a String, a Buffer, or an Array')
+          }
+        }
         var expected = tests[1]
         var result = parser(input)
         if (typeof expected === 'function') {
