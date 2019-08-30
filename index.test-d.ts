8daae26683a23fc5be54d07d41d3205485739fa2
Martin Štekl <martin.stekl@gmail.com>
2019-08-30 09:41:11 +0200

    update TS definitions to be in sync with implementation

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,21 +1,31 @@
 import * as types from '.';
-import { expectType } from 'tsd';
+import {expectType} from 'tsd';
 
 // builtins
 expectType<types.TypesBuiltins>(types.builtins);
 
-// getTypeParser
-const noParse = types.getTypeParser(types.builtins.NUMERIC, 'text');
-const numericParser = types.getTypeParser(types.builtins.NUMERIC, 'binary');
-expectType<string>(noParse('noParse'));
-expectType<number>(numericParser([200, 1, 0, 15]));
+// getTypeParser - not existing parser
+const noParse1 = types.getTypeParser<string>(types.builtins.NUMERIC);
+expectType<string>(noParse1('noParse'));
+const noParse2 = types.getTypeParser<string>(types.builtins.NUMERIC, 'text');
+expectType<string>(noParse2('noParse'));
+const noParse3 = types.getTypeParser<string>(types.builtins.BOOL, 'binary');
+expectType<string>(noParse3(Buffer.from([])));
 
-// getArrayParser 
-const value = types.arrayParser('{1,2,3}', (num) => parseInt(num));
-expectType<number[]>(value);
+// getTypeParser - existing parser
+const booleanParser1 = types.getTypeParser<boolean>(types.builtins.BOOL);
+expectType<boolean | string>(booleanParser1('t'));
+const booleanParser2 = types.getTypeParser<boolean>(types.builtins.BOOL, 'text');
+expectType<boolean | string>(booleanParser2('f'));
+const numericParser = types.getTypeParser<number>(types.builtins.NUMERIC, 'binary');
+expectType<number | string>(numericParser(Buffer.from([200, 1, 0, 15])));
 
-//setTypeParser
-types.setTypeParser(types.builtins.INT8, parseInt);
-types.setTypeParser(types.builtins.FLOAT8, parseFloat);
-types.setTypeParser(types.builtins.FLOAT8, 'binary', (data) => data[0]);
-types.setTypeParser(types.builtins.FLOAT8, 'text', parseFloat);
+// arrayParser
+const value = types.arrayParser.create('{1,2,3}', (num: string): number => Number.parseInt(num)).parse();
+expectType<ReadonlyArray<number>>(value);
+
+// setTypeParser
+types.setTypeParser(types.builtins.INT8, Number.parseInt);
+types.setTypeParser(types.builtins.FLOAT8, Number.parseFloat);
+types.setTypeParser(types.builtins.FLOAT8, 'binary', (data: Buffer): number => data[0]);
+types.setTypeParser(types.builtins.FLOAT8, 'text', Number.parseFloat);
